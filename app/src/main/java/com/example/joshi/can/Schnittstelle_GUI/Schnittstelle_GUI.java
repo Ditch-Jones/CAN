caf06626ee5c1f2942382fc3fc95c1d0fd6fed06
Alexander <lukacs.alexander@web.de>
2017-08-16 16:11:04 +0200

    Erstellung der Schnittstellen-Package

---
+++ app/src/main/java/com/example/joshi/can/Schnittstelle_GUI/Schnittstelle_GUI.java
@@ -0,0 +1,25 @@
+package com.example.joshi.can.Schnittstelle_GUI;
+
+import com.example.joshi.can.Logic.Node;
+
+/**
+ * Created by Besitzer on 16.08.2017.
+ */
+
+public class Schnittstelle_GUI {
+    Node node;
+    public static void main (String args[])
+    {
+
+    }
+
+    /**
+     * Der Aufruf für die GUI
+     */
+    public void placePicInCan()
+    {
+        node = new Node();
+        node.
+
+    }
+}
